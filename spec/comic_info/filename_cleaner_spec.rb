edfb348b24884f77167b1ae570ff460036f1920d
Shane Becker <veganstraightedge@gmail.com>
2025-11-11 18:04:40 -0800

    Replace YAML test loop with individual RSpec test cases

--- spec/comic_info/filename_cleaner_spec.rb
+++ spec/comic_info/filename_cleaner_spec.rb
@@ -1,21 +1,68 @@
 require 'spec_helper'
-require 'yaml'
 
 RSpec.describe ComicInfo::FilenameCleaner do
-  let(:test_cases) { YAML.load(fixture_file('filenames/test_cases.yaml'))['test_cases'] }
-
   describe '#clean' do
-    context 'with fixture test cases' do
-      it 'handles all test cases correctly' do
-        aggregate_failures do
-          test_cases.each do |test_case|
-            cleaner = described_class.new(tags: test_case['tags'])
-            result = cleaner.clean(test_case['input'])
+    context 'single tag removal' do
+      it 'removes Digital tag' do
+        cleaner = described_class.new(tags: ['Digital'])
 
-            expect(result).to eq(test_case['expected']),
-                              "Failed for '#{test_case['description']}': expected '#{test_case['expected']}' but got '#{result}'"
-          end
-        end
+        expect(cleaner.clean('Batman #1 (2021) (Digital).cbz')).to eq('Batman #1 (2021).cbz')
+      end
+    end
+
+    context 'multiple tag removal' do
+      it 'removes Digital, Zone-Empire, and c2c tags' do
+        cleaner = described_class.new(tags: %w[Digital Zone-Empire c2c])
+
+        expect(cleaner.clean('Amazing Spider-Man #1 (2018) (Digital) (Zone-Empire) (c2c).cbr')).to eq('Amazing Spider-Man #1 (2018).cbr')
+      end
+    end
+
+    context 'whitespace normalization' do
+      it 'normalizes multiple spaces and removes Digital tag' do
+        cleaner = described_class.new(tags: ['Digital'])
+
+        expect(cleaner.clean('  Superman    #1   (2021)   (Digital)  .cbz')).to eq('Superman #1 (2021).cbz')
+      end
+    end
+
+    context 'no tags to remove' do
+      it 'leaves filename unchanged when no matching tags' do
+        cleaner = described_class.new(tags: %w[Digital Scan])
+
+        expect(cleaner.clean('X-Men #1 (2019).cbz')).to eq('X-Men #1 (2019).cbz')
+      end
+    end
+
+    context 'partial tag match' do
+      it 'does not remove partial matches' do
+        cleaner = described_class.new(tags: ['Digital'])
+
+        expect(cleaner.clean('Flash #1 (2020) (Digital HD).cbz')).to eq('Flash #1 (2020) (Digital HD).cbz')
+      end
+    end
+
+    context 'case sensitive tags' do
+      it 'does not remove different case tags' do
+        cleaner = described_class.new(tags: ['Digital'])
+
+        expect(cleaner.clean('Wonder Woman #1 (2021) (digital).cbz')).to eq('Wonder Woman #1 (2021) (digital).cbz')
+      end
+    end
+
+    context 'complex filename with multiple spaces' do
+      it 'normalizes spaces and removes multiple tags' do
+        cleaner = described_class.new(tags: %w[Digital Zone-Empire c2c])
+
+        expect(cleaner.clean('Justice League   #1  (2018)   (Digital)  (Zone-Empire)   (c2c)  .cbr')).to eq('Justice League #1 (2018).cbr')
+      end
+    end
+
+    context 'special characters in tags' do
+      it 'removes tags with hyphens and special characters' do
+        cleaner = described_class.new(tags: %w[Digital-HD Zone-Empire])
+
+        expect(cleaner.clean('Deadpool #1 (2019) (Digital-HD) (Zone-Empire).cbz')).to eq('Deadpool #1 (2019).cbz')
       end
     end
 
