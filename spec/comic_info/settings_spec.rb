407fec85684417ec5d80ef79aadcfc4b0a9cbd75
Shane Becker <veganstraightedge@gmail.com>
2025-11-12 00:02:32 -0800

    Tidy

--- spec/comic_info/settings_spec.rb
+++ spec/comic_info/settings_spec.rb
@@ -20,10 +20,7 @@ RSpec.describe ComicInfo::Settings do
       before { described_class.create settings_path }
 
       it 'creates settings file with empty filename_tags' do
-        # described_class.create test_settings_path
-
         expect(File.exist?(test_settings_path)).to be true
-        # settings = YAML.load_file test_settings_path
         expect(filename_tags).to eq []
       end
     end
